64d266257382fa841e6e16e05c722878ba8a6aca
kimsangjoon82 <141383086+kimsangjoon82@users.noreply.github.com>
2023-11-22 12:54:50 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,26 +6,7 @@ import streamlit as st
 """
 # 유해물질 양산보증 법인별 측정현황!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+import streamlit as st
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-
-b = (
-    Bar()
-    .add_xaxis(["Microsoft", "Amazon", "IBM", "Oracle", "Google", "Alibaba"])
-    .add_yaxis("2017-2018 Revenue in (billion $)", random.sample(range(100), 10))
-    .set_global_opts(
-        title_opts=opts.TitleOpts(
-            title="Top cloud providers 2018", subtitle="2017-2018 Revenue"
-        ),
-        toolbox_opts=opts.ToolboxOpts(),
-    )
-)
-st_pyecharts(
-    b, key="echarts"
-)  # Add key argument to not remount component at every Streamlit run
-st.button("Randomize data")
-})
-"""
+st.title("This is a title")
+st.title("_Streamlit_ is :blue[cool] :sunglasses:")
